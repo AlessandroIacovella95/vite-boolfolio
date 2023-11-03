fc58c7b0e2b530215cd7ea6481333ea18824239c
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-11-03 19:23:52 +0100

    axios move in ProjectList

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <script>
 import ProjectList from "./components/projects/ProjectList.vue";
-import axios from "axios";
 
 export default {
   data() {
-    return {
-      projects: [],
-
-      api: {
-        baseUrl: "http://127.0.0.1:8000/api/",
-      },
-    };
+    return {};
   },
 
   components: { ProjectList },
-
-  methods: {
-    fetchProjects(uri = this.api.baseUrl + "projects") {
-      axios.get(uri).then((response) => {
-        this.projects = response.data.data;
-      });
-    },
-  },
-
-  created() {
-    this.fetchProjects();
-  },
 };
 </script>
 
 <template>
   <div class="container mt-4">
     <h1 class="mb-4">Boolfolio</h1>
-    <ProjectList :projects="projects" />
+    <ProjectList />
   </div>
 </template>
 
--- src/components/projects/ProjectList.vue
+++ src/components/projects/ProjectList.vue
@@ -1,15 +1,29 @@
 <script>
 import ProjectCard from "./ProjectCard.vue";
+import axios from "axios";
 
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+      api: {
+        baseUrl: "http://127.0.0.1:8000/api/",
+      },
+    };
   },
 
   components: { ProjectCard },
 
-  props: {
-    projects: Array,
+  methods: {
+    fetchProjects(uri = this.api.baseUrl + "projects") {
+      axios.get(uri).then((response) => {
+        this.projects = response.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
   },
 };
 </script>
